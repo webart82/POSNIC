7e4b0552748fbbd4f5904b67eee37e031e9934e3
vigneshposnic <vignesh.posnic@gmail.com>
2016-09-22 12:44:44 +0530

    #51_js_file_create

---
+++ js/add_customer.js
@@ -0,0 +1,46 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+        /*$.validator.setDefaults({
+         submitHandler: function() { alert("submitted!"); }
+         });*/
+        $(document).ready(function () {
+
+            // validate signup form on keyup and submit
+            $("#form1").validate({
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 3,
+                        maxlength: 200
+                    },
+                    address: {
+                        minlength: 3,
+                        maxlength: 500
+                    },
+                    contact1: {
+                        minlength: 3,
+                        maxlength: 20
+                    },
+                    contact2: {
+                        minlength: 3,
+                        maxlength: 20
+                    }
+                },
+                messages: {
+                    name: {
+                        required: "Please enter a Customer Name",
+                        minlength: "Customer must consist of at least 3 characters"
+                    },
+                    address: {
+                        minlength: "Customer Address must be at least 3 characters long",
+                        maxlength: "Customer Address must be at least 3 characters long"
+                    }
+                }
+            });
+
+        });
+
+

5661e7bf6379dd444ab88230d8089173b6600789
satheeshposnic <satheesh.posnic@gmail.com>
2016-10-25 10:35:22 +0530

    corresponding created js file in js folder

--- update_customer_details.php
+++ update_customer_details.php
@@ -19,73 +19,8 @@ include_once("init.php");
     <!-- jQuery & JS files -->
     <?php include_once("tpl/common_js.php"); ?>
     <script src="js/script.js"></script>
-    <script>
-        /*$.validator.setDefaults({
-         submitHandler: function() { alert("submitted!"); }
-         });*/
-        $(document).ready(function () {
-
-            // validate signup form on keyup and submit
-            $("#form1").validate({
-                rules: {
-                    name: {
-                        required: true,
-                        minlength: 3,
-                        maxlength: 200
-                    },
-                    address: {
-                        minlength: 3,
-                        maxlength: 500
-                    },
-                    contact1: {
-                        required: true,
-                        minlength: 10,
-                        maxlength: 10
-                    },
-                    contact2: {
-                        minlength: 3,
-                        maxlength: 20
-                    }
-                },
-                messages: {
-                    name: {
-                        required: "Please enter a supplier name",
-                        minlength: "Customer must consist of at least 3 characters"
-                    },
-                    contact1: {
-                        required: "Please enter a contact number",
-                        minlength: "Customer must consist of at least 10 characters"
-                    },
-                    address: {
-                        minlength: "Customer Address must be at least 3 characters long",
-                        maxlength: "Customer Address must be at least 3 characters long"
-                    }
-                }
-            });
-
-        });
-
-function numbersonly(e) {
-            var unicode = e.charCode ? e.charCode : e.keyCode
-            if (unicode != 8 && unicode != 46 && unicode != 37 && unicode != 27 && unicode != 38 && unicode != 39 && unicode != 40 && unicode != 9) { //if the key isn't the backspace key (which we should allow)
-                if (unicode < 48 || unicode > 57)
-                    return false
-            }
-        }
-
-function lettersOnly(evt) {
-       evt = (evt) ? evt : event;
-       var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
-          ((evt.which) ? evt.which : 0));
-       if (charCode > 31 && (charCode < 65 || charCode > 90) &&
-          (charCode < 97 || charCode > 122)) {
-          return false;
-       }
-       return true;
-     }	
-
-    </script>
-
+    <script src="js/update_customer_details.js"></script>
+    
 </head>
 <body>
 
